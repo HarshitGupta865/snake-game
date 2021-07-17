d0c1633a22f97b05fbf90ac836d97b00f91f6b2c
HarshitGupta865 <harshitkumargupta1117@gmail.com>
2021-07-17 13:20:03 +0530

    Moved the snake

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
 
-#To set the pop-up screen to required size
+# To set the pop-up screen to required size
 screen.setup(width=600, height=600)
 
-#to set the backgroung color of pop-up screen
+# to set the background color of pop-up screen
 screen.bgcolor("black")
 
 # to set title of the pop-up
 screen.title("My Snake Game")
 
 screen.tracer(0)
-
 
 # 1.--------------------------------- CREATING THE SNAKE BODY--------------------------------------
 
@@ -28,30 +28,31 @@ screen.tracer(0)
 # segment3.goto(-40, 0)
 
 
-#Instead of declaring three segments separately, we created a for loop
-starting_postions = [(0, 0), (-20, 0), (-40, 0)]
+# Instead of declaring three segments separately, we created a for loop
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 segments = []
 
-for position in starting_postions:
+for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
     new_segment.penup()
     new_segment.goto(position)
     segments.append(new_segment)
 
-
 # 2. ----------------------------------MAKING THE BODY OF SNAKE MOVE---------------------------------
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
+    # By this code we make the blocks of snake body follow the block that is ahead of it.
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 
